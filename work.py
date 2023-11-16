dad686af76045788adedf6a50d95d596ccce2ecb
BAB78 <146398248+BAB78@users.noreply.github.com>
2023-11-16 23:43:29 +0000

    Update work.py

--- work.py
+++ work.py
@@ -1,26 +1,99 @@
-def configure_syslog(ip, username, password, enable_password):
+import paramiko
+import os
+
+# Define variables for the menu options
+option_1 = "Telnet"
+option_2 = "SSH"
+option_3 = "Compare with startup configuration"
+option_4 = "Compare with local offline version"
+option_5 = "Compare with Cisco device hardening advice"
+option_6 = "Configure syslog for event logging and monitoring"
+option_7 = "Exit"
+
+# Define the IP address of the device to connect to
+ip_address = "192.168.0.1"
+
+# Define the username and password for the device
+username = "admin"
+password = "password"
+
+# Define the enable password for the device
+enable_password = "EnablePassword123"
+
+# Define the path to the hardening advice file
+hardening_advice_file = "/path/to/cisco/hardening/advice.txt"
+
+# Define a function for connecting to the device using Telnet
+def telnet_connection():
     try:
-        # Establish an SSH connection to configure syslog
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(ip, username=username, password=password)
-
-        # Command list to generate a 2048-bit RSA key for SSH
-        command_list = [
-            'configure terminal',
-            'crypto key generate rsa modulus 2048',  # Generating a 2048-bit RSA key
-            'end',
-            'write memory'
-        ]
-
-        # Execute commands only if the SSH connection is active
-        if ssh.get_transport() is not None and ssh.get_transport().is_active():
-            for command in command_list:
-                ssh.exec_command(command)
-
-            ssh.close()
-            print(f"RSA key (2048-bit) generated successfully for SSH on {ip}")
-        else:
-            print("SSH connection is not active.")
+        telnet = paramiko.Telnet(ip_address, username=username, password=password)
+        return telnet
     except Exception as e:
-        print(f"Failed to generate RSA key: {e}")
+        print(f"Error connecting to {ip_address} via Telnet: {e}")
+
+# Define a function for connecting to the device using SSH
+def ssh_connection():
+    try:
+        ssh = paramiko.SSH(ip_address, username=username, password=password)
+        return ssh
+    except Exception as e:
+        print(f"Error connecting to {ip_address} via SSH: {e}")
+
+# Define a function for comparing the current running configuration with the startup configuration
+def compare_configurations():
+    try:
+        running_config = get_running_configuration()
+        startup_config = get_startup_configuration()
+        diff = compare_configurations(running_config, startup_config)
+        print(diff)
+    except Exception as e:
+        print(f"Error comparing configurations: {e}")
+
+# Define a function for comparing the current running configuration with a local offline version
+def compare_with_local_offline_version():
+    try:
+        running_config = get_running_configuration()
+        local_config = get_local_offline_configuration()
+        diff = compare_configurations(running_config, local_config)
+        print(diff)
+    except Exception as e:
+        print(f"Error comparing configurations: {e}")
+
+# Define a function for comparing the current running configuration with Cisco device hardening advice
+def compare_with_hardening_advice():
+    try:
+        running_config = get_running_configuration()
+        advice = read_hardening_advice(hardening_advice_file)
+        diff = compare_configurations(running_config, advice)
+        print(diff)
+    except FileNotFoundError as e:
+        print(f"Error reading hardening advice file: {e}")
+    except Exception as e:
+        print(f"Error comparing configurations: {e}")
+
+# Define a function for configuring syslog for event logging and monitoring
+def configure_syslog():
+    try:
+        syslog_server_ip = input("Enter the IP of the syslog server: ")
+        configure_syslog_server(syslog_server_ip)
+    except Exception as e:
+        print(f"Error configuring syslog: {e}")
+
+# Define a function for displaying the menu and executing the selected option
+def display_menu():
+    while True:
+        print("\nMenu:")
+        print(f"{option_1}: Telnet")
+        print(f"{option_2}: SSH")
+        print(f"{option_3}: Compare with startup configuration")
+        print(f"{option_4}: Compare with local offline version")
+        print(f"{option_5}: Compare with Cisco device hardening advice")
+        print(f"{option_6}: Configure syslog for event logging and monitoring")
+        print(f"{option_7}: Exit")
+
+        choice = input("Enter your choice (1-7): ")
+
+        if choice == option_1:
+            telnet_connection()
+        elif choice == option_2:
+            ssh_connection()
